18bc08cb1ece2fdca4e073eeb1972187a8a06564
holliemaethomas <holliemaethomas85@gmail.com>
2018-01-09 20:46:02 -0800

    index.html working

---
+++ js/app.js
@@ -0,0 +1,29 @@
+'use strict';
+ var greeting = prompt ('Hello again, I really missed you')
+console.log('The user entered:' + greeting);
+var response1 = prompt('wanna hang out and play a quick trivia game? yes or no?').toUpperCase(); 
+ 
+ console.log ('the user entered:' + response1)
+
+ if (response1 === 'yes') {
+     alert('sweeeeet!');
+ } 
+ else if (response1 === 'no') {
+     alert ('lets do it anyway!'); 
+ }
+
+//var question1 = prompt('Yo baby whats up?')
+//console.log('The user entered:' + greeting);
+//var response1 = prompt('Me too! wanna hang and play a quick trivia game? yes or no?')
+//console.log('the user entered:' + response1)
+
+//if (response1 === 'yes') {
+    //alert('sweeeeet!');
+//}
+//else if (response1 === no) {
+    //alert('lets do it anyway!');
+//}
+ 
+
+
+
